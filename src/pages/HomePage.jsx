cd9bbbc38a7f7bc2c9303dbc9bab7cd4b9f2ccd9
Rudra Pratap Singh <chaudhary66667@gmail.com>
2025-01-17 10:28:35 +0530

    +Swiper.js

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import imagePoloroid from '../assets/poloroid_1.png'
+import imagePoloroid2 from '../assets/poloroid_2.png'
+import imagePoloroid3 from '../assets/poloroid_3.png'
+import imagePoloroid4 from '../assets/poloroid_4.png'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
 
 const HomePage = () => {
     return (
         <section className='p-4 flex flex-col gap-4'>
             <h1 className='font-lexend text-5xl tracking-tighter font-semibold text-yellow-950'>Hi there! <br /> I'm Rudra.</h1>
             <aside>
-                <img src={imagePoloroid} alt='Rudra Pratap Singh' />
+                <Swiper
+                    effect={'cards'}
+                    grabCursor={true}
+                    modules={[EffectCards]}
+                    cardsEffect={
+                        {
+                            slideShadows: false,
+                        }
+                    }
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src={imagePoloroid} alt='Rudra Pratap Singh' />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={imagePoloroid2} alt='Rudra Pratap Singh' />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={imagePoloroid3} alt='Rudra Pratap Singh' />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={imagePoloroid4} alt='Rudra Pratap Singh' />
+                    </SwiperSlide>
+                </Swiper>
             </aside>
         </section>
     );
