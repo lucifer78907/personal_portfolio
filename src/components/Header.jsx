cf6bf49b6ba3992383dad6c1d67cee1d5af8bba2
Rudra Pratap Singh <chaudhary66667@gmail.com>
2025-01-18 08:23:50 +0530

    +(Header)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className='bg-red-200 p-2'>
-            <h1>Under serious development</h1>
+        <header className='bg-amber-100/30 backdrop-blur-md m-4 p-2 flex justify-evenly  items-center rounded-full sticky top-2 z-50 shadow-md'>
+            <NavLink to='/' className='text-base font-bold text-amber-50 bg-amber-800 px-4 rounded-full'>Home</NavLink>
+            <NavLink to='/' className='text-base font-bold text-amber-800'>Gallery</NavLink>
+            <NavLink to='/' className='text-base font-bold text-amber-800'>Contact</NavLink>
+            <NavLink to='/' className='text-base font-bold text-amber-800'>Blog</NavLink>
         </header>
     );
 };
