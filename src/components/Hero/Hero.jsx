f12d3d1eba225b792e5307b845716b791ee013b9
Rudra Pratap Singh <chaudhary66667@gmail.com>
2025-05-06 01:35:07 +0530

    (Gsap added)

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Poloroids from './Poloroids';
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/all';
+import gsap from 'gsap';
+
+const description = [
+    {
+        text: 'travel || code || fun',
+        color: 'text-amber-800'
+    },
+    {
+        text: 'Error at line 98:57',
+        color: 'text-red-500'
+    },
+    {
+        text: 'humorous',
+        color: 'text-amber-800'
+    },
+    {
+        text: 'coffee && gym addict',
+        color: 'text-amber-800'
+    },
+]
 
 const Hero = () => {
-    const description = [
-        {
-            text: 'travel || code || fun',
-            color: 'text-amber-800'
-        },
-        {
-            text: 'Error at line 98:57',
-            color: 'text-red-500'
-        },
-        {
-            text: 'humorous',
-            color: 'text-amber-800'
-        },
-        {
-            text: 'coffee && gym addict',
-            color: 'text-amber-800'
-        },
-    ]
+    const containerRef = useRef();
+    gsap.registerPlugin(SplitText);
+
+    useGSAP(() => {
+        let split = SplitText.create('.heading', {
+            type: 'chars',
+            mask: 'chars',
+            onSplit: (self) => {
+                let split = gsap.from(self.chars, {
+                    duration: 0.6,
+                    yPercent: 100,
+                    opacity: 0,
+                    stagger: 0.1,
+                    ease: "expo.out",
+                });
+                return split;
+            }
+        })
+    }, [])
+
 
     return (
-        <section className='p-4 flex flex-col mb-12 xl:mt-4'>
-            <h1 className='font-lexend text-5xl sm:text-6xl md:text-7xl xl:text-9xl xl:text-center tracking-tighter  font-semibold text-yellow-950'>Hi there! <br /> I'm Rudra.</h1>
+        <section ref={containerRef} className='p-4 flex flex-col mb-12 xl:mt-4' >
+            <h1 className='heading font-lexend text-5xl sm:text-6xl md:text-7xl xl:text-9xl xl:text-center tracking-tighter  font-semibold text-yellow-950'>Hi there! <br /> I'm Rudra.</h1>
             <p className='mt-4 font-lexend sm:text-lg lg:text-xl xl:text-center xl:text-2xl text-amber-700 text-xs'>{`<FullStackDev/>`} turning ideas into fast, scalable web apps with real impact.</p>
             <div className='xl:grid xl:grid-cols-2 xl:gap-8 2xl:gap-16 xl:items-center'>
                 <Poloroids />
@@ -35,7 +59,7 @@ const Hero = () => {
                     }
                 </p>
             </div>
-        </section>
+        </section >
     );
 };
 
